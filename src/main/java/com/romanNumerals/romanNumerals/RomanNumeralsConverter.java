a05e5639c05193ef4cc2a6623a092eb6665b9cb2
jordancodurance <jordan.colgan@codurance.com>
2021-09-07 11:22:58 +0100

    Complete other special cases for roman numeral converter

--- src/main/java/com/romanNumerals/romanNumerals/RomanNumeralsConverter.java
+++ src/main/java/com/romanNumerals/romanNumerals/RomanNumeralsConverter.java
@@ -2,48 +2,39 @@ package com.romanNumerals.romanNumerals;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
 import java.util.TreeMap;
 
 @SpringBootApplication
 public class RomanNumeralsConverter {
 
-	private final TreeMap<Integer, String> specialCaseNumerals = new TreeMap<>() {{
-		put(1, "I");
-		put(4, "IV");
-		put(5, "V");
-		put(9, "IX");
-		put(10, "X");
-		put(40, "XL");
-		put(50, "L");
-		put(100, "C");
-		put(500, "D");
-		put(1000, "M");
+    private final TreeMap<Integer, String> specialCaseNumerals = new TreeMap<>() {{
+        put(1, "I");
+        put(4, "IV");
+        put(5, "V");
+        put(9, "IX");
+        put(10, "X");
+        put(40, "XL");
+        put(50, "L");
+        put(90, "XC");
+        put(100, "C");
+        put(400, "CD");
+        put(500, "D");
+        put(900, "CM");
+        put(1000, "M");
+    }};
 
-	}};
+    public String convert(int amount) {
+        String numeralFound = specialCaseNumerals.get(amount);
+        if (numeralFound != null) return numeralFound;
 
-	public String convert(int amount) {
+        StringBuilder numeral = new StringBuilder();
+        while (amount != 0) {
+            Integer nearestAmount = specialCaseNumerals.floorKey(amount);
+            numeral.append(specialCaseNumerals.get(nearestAmount));
+            amount -= nearestAmount;
+        }
 
-		String numeralFound = specialCaseNumerals.get(amount);
-
-		if(numeralFound != null) return numeralFound;
-		String numeral = "";
-		while (amount != 0) {
-			Integer nearestAmount = specialCaseNumerals.floorKey(amount);
-			numeral += specialCaseNumerals.get(nearestAmount);
-			amount -= nearestAmount;
-		}
-/*		if (amount > 5) {
-			numeral += specialCaseNumerals.get(amount -1);
-			numeral += specialCaseNumerals.get(1);
-		} else {
-			for(int i = 1; i <= amount; i++) {
-				numeral += specialCaseNumerals.get(1);
-			}
-		}*/
-
-		return numeral;
-	}
+        return numeral.toString();
+    }
 
 }
-
