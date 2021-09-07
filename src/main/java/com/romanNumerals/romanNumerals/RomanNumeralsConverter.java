c5b48d808f2418eed252a6206fb528bcf8e03caf
Isabel Martínez <i.m.martinez@hotmail.es>
2021-09-07 12:10:55 +0200

    complete conversion roman numerals

--- src/main/java/com/romanNumerals/romanNumerals/RomanNumeralsConverter.java
+++ src/main/java/com/romanNumerals/romanNumerals/RomanNumeralsConverter.java
@@ -3,14 +3,18 @@ package com.romanNumerals.romanNumerals;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 
 @SpringBootApplication
 public class RomanNumeralsConverter {
 
-	private final HashMap<Integer, String> specialCaseNumerals = new HashMap<>() {{
+	private final TreeMap<Integer, String> specialCaseNumerals = new TreeMap<>() {{
 		put(1, "I");
+		put(4, "IV");
 		put(5, "V");
+		put(9, "IX");
 		put(10, "X");
+		put(40, "XL");
 		put(50, "L");
 		put(100, "C");
 		put(500, "D");
@@ -24,9 +28,19 @@ public class RomanNumeralsConverter {
 
 		if(numeralFound != null) return numeralFound;
 		String numeral = "";
-		for(int i = 1; i <= amount; i++) {
-			numeral += specialCaseNumerals.get(1);
+		while (amount != 0) {
+			Integer nearestAmount = specialCaseNumerals.floorKey(amount);
+			numeral += specialCaseNumerals.get(nearestAmount);
+			amount -= nearestAmount;
 		}
+/*		if (amount > 5) {
+			numeral += specialCaseNumerals.get(amount -1);
+			numeral += specialCaseNumerals.get(1);
+		} else {
+			for(int i = 1; i <= amount; i++) {
+				numeral += specialCaseNumerals.get(1);
+			}
+		}*/
 
 		return numeral;
 	}
